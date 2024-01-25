77048042e5d8fbd6ffd0c9970c31668d58a7f48b
Huertz <104065687+Huertz@users.noreply.github.com>
2024-01-24 23:22:48 -0600

    added same file to to some test

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from './StarRating.jsx';
-// import App from './App.jsx';
-// import './index.css';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
-    <StarRating
-      size={24}
-      color='red'
-      className='test'
-    />
+    <App />
   </React.StrictMode>
 );
