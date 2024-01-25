4bc688720300c9ffcf66b23e124c23d1c6fef4ee
Huertz <104065687+Huertz@users.noreply.github.com>
2024-01-25 00:21:12 -0600

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -52,17 +52,18 @@ const KEY = 'f7d5f8df';
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'batman';
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
-      console.log(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -74,9 +75,7 @@ export default function App() {
         <NumResults movies={movies} />
       </Navbar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loaded /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
@@ -88,6 +87,10 @@ export default function App() {
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+function Loaded() {
+  return <p className='loader'>Loading...</p>;
+}
 
 function Navbar({ children }) {
   return (
