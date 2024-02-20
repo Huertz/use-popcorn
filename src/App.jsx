986beba41660afc2eacfdc06fd35ab24d3bee3da
Huertz <104065687+Huertz@users.noreply.github.com>
2024-02-20 00:34:38 -0600

    showing movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
+import StarRating from './StarRating';
 
 const tempMovieData = [
   {
@@ -255,14 +256,71 @@ function Movies({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  console.log(title, year);
+  useEffect(function () {
+    async function getMoiveDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      setMovie(data);
+    }
+    getMoiveDetails();
+  }, []);
+
   return (
     <div className='details'>
-      <button
-        className='btn-back'
-        onClick={onCloseMovie}
-      >
-        &larr;
-      </button>
+      <header>
+        <button
+          className='btn-back'
+          onClick={onCloseMovie}
+        >
+          &larr;
+        </button>
+        <img
+          src={poster}
+          alt={`Poster of the ${movie}`}
+        />
+        <div className='details-overview'>
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐️</span>
+            {imdbRating}
+          </p>
+        </div>
+      </header>
+      <section>
+        <div className='rating'>
+          <StarRating
+            maxRating={10}
+            size={24}
+          />
+        </div>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
       {selectedId}
     </div>
   );
